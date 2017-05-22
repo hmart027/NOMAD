1c634fd7672b059fa31df612213c8e3fe33f7b8d
Harold Martin <harold.martin92@gmail.com>
2017-05-22 18:53:51 -0400

    General Changes to CameraControl

--- src/edu/fiu/cate/nomad/vision/CameraControl.java
+++ src/edu/fiu/cate/nomad/vision/CameraControl.java
@@ -5,11 +5,16 @@ import com.protocol.Protocol;
 import comm.serial.Comm;
 import comm.serial.InputStreamListener;
 import joystick.DS4;
+import math2.Math2;
 
 public class CameraControl {
 	DS4 controller;
 	Comm serial;
 	Protocol protocol;
+	
+	public final float CAMERA_OFFSET_THETA 	= 90;
+	public final float CAMERA_OFFSET_PHI 	= 90;
+	public final float CAMERA_OFFSET_X 		= 0.08f;
 	
 	public CameraControl(){
 		
@@ -30,17 +35,35 @@ public class CameraControl {
 		}
 		serial.getComm("/dev/ttyUSB0", 9600);
 		
+		double rX, rY, lY;
+		int rH, rV, lH, lV;
+		float[] rCoordiantes, lCoordinates;
 		while(true){
-//			System.out.println("rx:" + controller.getRX());
-//			System.out.println("ry:" + controller.getRY());
-//			System.out.println("ly:" + controller.getLY());
+			rX = controller.getRX()*90;
+			rY = controller.getRY()*90;
+			lY = (-controller.getLY()+1)*2.5+.02;
+						
+			rCoordiantes = Math2.spherical2cartesian((float)lY, (float)rX + CAMERA_OFFSET_THETA, (float)rY + CAMERA_OFFSET_PHI);
+			lCoordinates = Math2.cartesian2spherical(rCoordiantes[0]-CAMERA_OFFSET_X, rCoordiantes[1], rCoordiantes[2]);
+
+//			System.out.println("rX:" + rCoordiantes[0]);
+//			System.out.println("rY:" + rCoordiantes[1]);
+//			System.out.println("rZ:" + rCoordiantes[2]);
 //			System.out.println();
-			int rX = (int) (controller.getRX()*90);
-			int rY = (int) (controller.getRY()*90);
-			int lX = (int) (controller.getRX()*90);
-			int lY = (int) (controller.getRY()*90);
-			System.out.println("cy:" + rY);
-			serial.sendByteArray(Protocol.pack(new byte[]{0, (byte) rX, (byte) rY, (byte) lX, (byte) lY}));
+//			System.out.println("r-X :" + lY);
+//			System.out.println("r-Y :" + lCoordinates[0]);
+//			System.out.println("th-X:" + rX);
+//			System.out.println("th-Y:" + (lCoordinates[1] - CAMERA_OFFSET_THETA));
+//			System.out.println("ph-X:" + rY);
+//			System.out.println("ph-Y:" + (lCoordinates[2] - CAMERA_OFFSET_PHI));
+//			System.out.println();
+			
+			rH = (int) (rX);
+			rV = (int) (rY);
+			lH = (int) (lCoordinates[1] - CAMERA_OFFSET_THETA);
+			lV = (int) (lCoordinates[2] - CAMERA_OFFSET_PHI);
+						
+			serial.sendByteArray(Protocol.pack(new byte[]{0, (byte) rH, (byte) rV, (byte) lH, (byte) lV}));
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
